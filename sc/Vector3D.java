a16bec0e8c88e2585d3dd44782fe71c195bfe4bb
FrancisYaoQin <qin.f@northeastern.edu>
2025-01-09 16:03:20 -0800

    add a constructor that takes in x,y,z components. As well as methods of the individual components

---
+++ sc/Vector3D.java
@@ -0,0 +1,32 @@
+package sc;
+
+public class Vector3D {
+
+    public static void main(String[] args) {
+
+    }
+
+    private double x;
+    private double y;
+    private double z;
+
+    //Q1
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //Q2
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    
+    public double getZ() {
+        return z;
+    }
+}
